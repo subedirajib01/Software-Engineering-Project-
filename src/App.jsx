af7e0f33a2dbe13e251ce3390dcde0fad0573737
Rajib Subedi <subedirajib59@gmail.com>
2025-07-30 22:21:05 +0545

    adding all the files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from './components/Navbar/navbar';
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/assets/assets.js
@@ -0,0 +1,135 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import basket_icon from './basket_icon.png'
+import add_icon_green from './add_icon_green.png'
+import add_icon_white from './add_icon_white.png'
+import app_store from './app_store.png'
+import bag_icon from './bag_icon.png'
+import cross_icon from './cross_icon.png'
+import facebook_icon from './facebook_icon.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+import header_img from './header_img.png'
+import linkedin_icon from './linkedin_icon.png'
+import logout_icon from './logout_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import play_store from './play_store.png'
+import profile_icon from './profile_icon.png'
+import rating_starts from './rating_starts.png'
+import remove_icon_red from './remove_icon_red.png'
+import search_icon from './search_icon.png'
+import selector_icon from './selector_icon.png'
+import twitter_icon from './twitter_icon.png'
+
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    basket_icon, 
+    profile_image,
+    add_icon_green,
+    add_icon_white,
+    food_1,
+    food_10,
+    food_11,
+    food_12,
+    food_13,
+    food_14,
+    food_15,
+    food_16,
+    food_18,
+    food_19,
+    food_2,
+    food_20,
+    food_21,
+    food_22,
+    food_23,
+    food_24,
+    food_25,
+    food_26,
+    food_27,
+    food_28,
+    food_29,
+    food_30,
+    food_31,
+    food_32,
+    food_4,
+    food_5,
+    food_6,
+    food_7,
+    food_8,
+    food_9,
+    food_3,
+    food_17,
+    app_store,
+    bag_icon,
+    cross_icon,
+    facebook_icon,
+    menu_1,
+    menu_2,
+    menu_3,
+    menu_4,
+    menu_5,
+    menu_6,
+    menu_7,
+    menu_8,
+    play_store,
+    profile_icon,
+    rating_starts,
+    remove_icon_red,
+    search_icon,
+    selector_icon,
+    twitter_icon,
+    header_img,
+    linkedin_icon,
+    logout_icon
+
+}
+
+export const url = 'http://localhost:4000'
